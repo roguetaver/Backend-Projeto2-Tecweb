2499b3153fe5b303313d43c72b8031dd0070fd07
roguetaver <andretavernaro@gmail.com>
2020-11-12 23:49:52 -0300

    tentantiva deploy

--- app.js
+++ app.js
@@ -25,3 +25,10 @@ app.listen(port, () => {
 });
 
 module.exports = app
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
